161c6c524a7e1a026cc8e6d093111e704aa6f473
Dmitry Ryzhkin <ryshkin@gmail.com>
2015-06-02 00:05:58 +0300

    added calc statistic for texts

--- service/do.php
+++ service/do.php
@@ -61,7 +61,7 @@
 
 
 class ServiceDo {
-    //public static $test1 = "rrrr";
+    public static $prepareText = "Splitting and preparing text to the words. Returned array of words.";
     public static function prepareText($str, $minWordLen = 0) {
         $str = str_replace(array("\r\n", "\r", "\n", ".", ",", ":", "'", '"', "(", ")", "[", "]", "{", "}", "?", "!"), '', $str);
         $str = mb_convert_case($str, MB_CASE_UPPER, "UTF-8");
@@ -87,7 +87,7 @@ class ServiceDo {
         );
     }
 
-    public static function getFrequencies($text){
+    public static function getFrequencies($text) {
         // Удалим все кроме букв
         //$text = preg_replace("/[^\p{L}]/iu", "", strtolower($text));
 
@@ -110,27 +110,27 @@ class ServiceDo {
         $morphyRU = new phpMorphy(__DIR__.'/lib/phpmorphy/dicts/ru2', 'ru_RU', array(
           'storage' => PHPMORPHY_STORAGE_FILE,
         ));
+        $morphyEN = new phpMorphy(__DIR__.'/lib/phpmorphy/dicts/en1', 'en_EN', array(
+          'storage' => PHPMORPHY_STORAGE_FILE,
+        ));
 
         //echo $morphyRU->getEncoding()."\n";
         //echo $morphyRU->getLocale()."\n";
         $src = "Hi my dear friend. How are you? Что же умеет эта библиотека? Для начала хотел обратить внимание что можно получить корень слова, морфологические формы слова, определение частей речи, грамматические формы. Я думаю многие разработчики понимают пользу таких преобразований, например, есть возможность улучшить поиск в своей системе, если получать корень слова и искать уже по нему. В данном случае моя задача состояла сбор анкоров в СЕО системе, учитывая морфологию слов.\n";
         $preparedText = self::prepareText($src, 4);
-        $lemmas       = $morphyRU->getBaseForm($preparedText['ru']);
-
+        $lemmasRU       = $morphyRU->getBaseForm($preparedText['ru']);
+        $lemmasEN       = $morphyEN->getBaseForm($preparedText['en']);
         //tool::clog($src);
         tool::clog($preparedText);
-        tool::clog($lemmas);
+        tool::clog($lemmasRU);
+        tool::clog($lemmasEN);
 
 
 
-        //tool::clog($morphy->getGramInfoMergeForms('БИБЛИОТЕКА'));
-        //tool::clog($morphy->getGramInfoMergeForms('НЕ'));
-        //tool::clog($morphy->getGramInfoMergeForms(array('БИБЛИОТЕКА', 'НЕ')));
-
-
-        //tool::clog(self::getFrequencies($src));
-
-
+        // tool::clog($morphy->getGramInfoMergeForms('БИБЛИОТЕКА'));
+        // tool::clog($morphy->getGramInfoMergeForms('НЕ'));
+        // tool::clog($morphy->getGramInfoMergeForms(array('БИБЛИОТЕКА', 'НЕ')));
+        // tool::clog(self::getFrequencies($src));
 
         // http://php.net/manual/en/function.fann-create-standard.php
         //$ann = fann_create_standard(3, 256, 128, 3);
@@ -140,7 +140,115 @@ class ServiceDo {
       }
     }
 
+    private static function sortStatistic(&$words) {
+       uasort($words, function ($a, $b) {
+          if ($a['count'] == $b['count']) {
+              return 0;
+          }
+          return ($a['count'] < $b['count']) ? -1 : 1;
+      });
+      return $words;
+    }
+    private static function countWords($text = '') {
+       $text = ($text != '')?$text:"Hi my dear friend. How are you? Что же умеет эта библиотека? Для начала хотел обратить внимание что можно получить корень слова, морфологические формы слова, определение частей речи, грамматические формы. Я думаю многие разработчики понимают пользу таких преобразований, например, есть возможность улучшить поиск в своей системе, если получать корень слова и искать уже по нему. В данном случае моя задача состояла сбор анкоров в СЕО системе, учитывая морфологию слов.\n";
+       $preparedText = self::prepareText($text);
+       $ru = array();
+       $en = array();
+       require_once('lib/phpmorphy/src/common.php');
+       try {
+            $morphyRU = new phpMorphy(__DIR__.'/lib/phpmorphy/dicts/ru2', 'ru_RU', array(
+                'storage' => PHPMORPHY_STORAGE_FILE,
+            ));
+            $morphyEN = new phpMorphy(__DIR__.'/lib/phpmorphy/dicts/en1', 'en_EN', array(
+                'storage' => PHPMORPHY_STORAGE_FILE,
+            ));
+            $lemmasRU       = $morphyRU->getBaseForm($preparedText['ru']);
+            foreach ($preparedText['ru'] as $word) {
+               if (isset($lemmasRU[$word]) && count($lemmasRU[$word]) > 0) {
+                   if (isset($ru[$lemmasRU[$word][0]])) {
+                       $ru[$lemmasRU[$word][0]]['count']++;
+                   } else {
+                       $ru[$lemmasRU[$word][0]] = array(
+                           'count' => 1,
+                       );
+                   }
+               }
+           }
+           $lemmasEN  = $morphyEN->getBaseForm($preparedText['en']);
+           foreach ($preparedText['en'] as $word) {
+               if (isset($lemmasEN[$word]) && count($lemmasEN[$word]) > 0) {
+                   if (isset($en[$lemmasEN[$word][0]])) {
+                       $en[$lemmasEN[$word][0]]['count']++;
+                   } else {
+                       $en[$lemmasEN[$word][0]] = array(
+                           'count' => 1,
+                       );
+                   }
+               }
+           }
+       } catch(phpMorphy_Exception $e) {
+           die('Error occured while creating phpMorphy instance: ' . $e->getMessage());
+       }
+       $statistic = array(
+         'en' => self::sortStatistic($en),
+         'ru' => self::sortStatistic($ru),
+       );
+       //tool::clog($statistic);
+       return $statistic;
+    }
+    private static function addStatistic($s1, $s2) {
+      foreach ($s2 as $w2 => $v2) {
+        if (isset($s1[$w2]) && isset($s1[$w2]['count']) && isset($v2['count'])) {
+          $s1[$w2]['count'] +=  $v2['count'];
+        } else {
+          $s1[$w2]['count'] =  $v2['count'];
+        }
+      }
+      return $s1;
+    }
+    public static function getStatisticForTexts ($texts = array()) {
+        $texts = array(
+          "Назначение Михеила Саакашвили председателем Одесской ОГА привлекло внимание за рубежом. Об этом заявил президент Украины Петр Порошенко в ходе встречи с председателем Одесской ОГА Михеилом Саакашвили, сообщили в пресс-службе главы государства.",
+          "Почти 60 тонн сыра, который перевозился без необходимых документов, не пустили в Россию из Казахстана, сообщается на сайте Россельхознадзора.",
+          "Почти В Японии в результате падения 700-килограммового воздушного змея пострадали четыре человека, сообщает The Guardian.",
+          "Почти Атомная электростанция в Бушере заработала в 2011 г. при содействии РФ. Она была спроектирована так, что выдерживает землетрясение магнитудой 8.",
+        );
+        $ru = array();
+        $en = array();
+        foreach ($texts as $text) {
+          $s = self::countWords($text);
+          $ru = self::addStatistic($ru, $s['ru']);
+          $en = self::addStatistic($en, $s['en']);
+        }
+        $minStatCount = 2;
+
+        $ru_out = array();
+        foreach ($ru as $w => $v) {
+          if ($w !== '' && $v['count'] >= $minStatCount) {
+            $ru_out[$w] = $v;
+          }
+        }
+
+        $en_out = array();
+        foreach ($en as $w => $v) {
+            if ($w !== '' && $v['count'] >= $minStatCount) {
+                $en_out[$w] = $v;
+            }
+        }
+
+        $statistic = array(
+          'en' => self::sortStatistic($en_out),
+          'ru' => self::sortStatistic($ru_out),
+        );
+        tool::clog($statistic);
+        return $statistic;
+    }
+
+
 }
+
+
+
 
 
 
