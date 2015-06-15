0b17573135d5935d9fd0b02906cd1631e31f56ae
Dmitry Ryzhkin <ryshkin@gmail.com>
2015-06-15 19:00:17 +0300

    added bootstrap

--- js/texts.js
+++ js/texts.js
@@ -2,16 +2,20 @@ var texts = angular.module('texts', []);
 
 texts.controller('texts.list', ['$scope', '$http',
     function ($scope, $http) {
-        $scope.texts = [1, 2, 3, 4, 5, 6, 7];
-
-/* $http.get('data/texts_bad.json').success(function(data) {
-         $scope.texts = data;
-         });
-         */
-
-        $scope.orderProp = 'age';
-        console.log('+++');
+        $scope.texts = [];
+        $http.get('data/texts_bad.json').success(function(data) {
+          $scope.texts = data;
+        });
+        $scope.order = true;
     }]);
+
+texts.filter('getDateTimeFromMySQL', function() {
+    return function(input) {
+        var t = input.split(/[- :]/);
+        var d = new Date(t[0], t[1]-1, t[2], t[3], t[4], t[5]);
+        return d.getTime();
+    };
+});
 
 /*
 texts.controller('TextsDetailCtrl', ['$scope', '$routeParams',
