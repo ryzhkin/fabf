70b47e1367cd0cddbe79be97789e7889ec596556
Dmitry Ryzhkin <ryshkin@gmail.com>
2015-06-18 23:32:03 +0300

    finished pagination

--- js/main.js
+++ js/main.js
@@ -6,15 +6,33 @@ var fabfApp = angular.module('fabfApp', [
 fabfApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
-            when('/texts', {
-                templateUrl : 'partials/texts-list.html',
-                controller  : 'texts.list'
+            when('/texts/:page', {
+                templateUrl    : 'partials/texts-list.html',
+                controller     : 'texts.list',
+                reloadOnSearch : false
             }).
           /*  when('/texts/:id', {
                 templateUrl: 'partials/texts-detail.html',
                 controller: 'TextsDetailCtrl'
             }).*/
             otherwise({
-                redirectTo: '/texts'
+                redirectTo: '/texts/1'
             });
     }]);
+
+
+
+// Добавляем возможность изменять путь без перезагрузки
+fabfApp.run(['$route', '$rootScope', '$location', function ($route, $rootScope, $location) {
+    var original = $location.path;
+    $location.path = function (path, reload) {
+        if (reload === false) {
+            var lastRoute = $route.current;
+            var un = $rootScope.$on('$locationChangeSuccess', function () {
+                $route.current = lastRoute;
+                un();
+            });
+        }
+        return original.apply($location, [path]);
+    };
+}]);
--- js/texts.js
+++ js/texts.js
@@ -1,9 +1,9 @@
 var texts = angular.module('texts', []);
 
-texts.controller('texts.list', ['$scope', '$http',
-    function ($scope, $http) {
+texts.controller('texts.list', ['$scope', '$http', '$location', '$routeParams',
+    function ($scope, $http, $location, $routeParams) {
         $scope.texts = [];
-        $scope.page = 1;
+        $scope.page = $routeParams.page;
         $scope.pages = [];
         $scope.countNavPagesView = 9;
         /*$http.get('data/texts_bad.json').success(function(data) {
@@ -13,24 +13,33 @@ texts.controller('texts.list', ['$scope', '$http',
 
         $scope.order = true;
 
+
+        $scope.showFullText = function (index) {
+          //console.log(index);
+          jQuery('.short-text').show();
+          jQuery('.full-text').hide();
+          jQuery('.short-text[index=' + index + ']').hide();
+          jQuery('.full-text[index=' + index + ']').slideDown();
+        }
+
         $scope.getDataPage = function(page) {
+            $location.path('/texts/' + page, false);
             $scope.page = page;
             $http.post('service/ajax.php', {
                 ajaxAction : 'getTexts',
                 page       : $scope.page
             }).
                 success(function(data, status, headers, config) {
-                    console.log(data);
+                    //console.log(data);
                     $scope.texts = data.texts;
                     var countPage = Math.floor(data.count/data.pageSize);
-                    var minPage = data.page - $scope.countNavPagesView + 5;
+
+
+                    var minPage = data.page - Math.floor($scope.countNavPagesView/2);
                     //minPage = (((data.page % $scope.countNavPagesView) == 0)?(data.page - Math.floor($scope.countNavPagesView/2)):0);
                     minPage = ((minPage > 0)?minPage:1);
-
                     var maxPage = minPage + $scope.countNavPagesView;
                     maxPage = ((maxPage < countPage)?maxPage:countPage);
-
-
 
 
                     $scope.pages = [];
@@ -46,18 +55,58 @@ texts.controller('texts.list', ['$scope', '$http',
                             class: (data.page == i)?'btn-success':'btn-default'
                         });
                     }
+
+                    /*
+                    if (data.page == maxPage) {
+                        for (var i = maxPage + 1; (i <= maxPage + Math.floor($scope.countNavPagesView/2)) && i < countPage ; i++) {
+                            $scope.pages.push({
+                                page:   i,
+                                class: 'btn-default btn-hide',
+                                hide: true
+                            });
+                        }
+                        setTimeout(function () {
+                            var serialShow = function (i, limit) {
+                                jQuery('button[page=' + i + ']').fadeIn('fast', function () {
+                                  i++;
+                                  if (i <= limit) {
+                                     serialShow(i, limit);
+                                  }
+                                });
+                            }
+
+                            var lastB = maxPage + Math.floor($scope.countNavPagesView/2);
+                            lastB = (lastB < countPage)?lastB:countPage;
+                            serialShow(maxPage + 1, lastB);
+
+                            var serialHide = function (i, limit) {
+                                jQuery('button[page=' + i + ']').fadeOut('fast', function () {
+                                    i++;
+                                    if (i <= limit) {
+                                        serialHide(i, limit);
+                                    }
+                                });
+                            }
+
+                            var lastC = minPage + Math.floor($scope.countNavPagesView/2);
+                            lastC = (lastC < countPage)?lastC:countPage;
+                            serialHide(minPage, lastC);
+                        }, 1000);
+                    }
+                    //*/
+
                     if (maxPage < countPage) {
                         $scope.pages.push({
                             page:   countPage,
                             class:  'btn-primary'
                         });
                     }
-                    console.log($scope.pages);
+                    //console.log($scope.pages);
                 }).
                 error(function(data, status, headers, config) {
                 });
         };
-        $scope.getDataPage(1);
+        $scope.getDataPage($scope.page);
 
 
     }]);
